df39bf06eba3197b11b205c40b15190e0f5dad02
André Silva <andre_vss@outlook.com>
2025-04-24 16:00:33 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Models/Categorie.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalogo.Models
+{
+    public class Categorie
+    {
+        public int CategorieId { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
