4ff27e1d3a7d1faf792750eaa4d824efc5ab68ac
andrevisilva <andrevisilva@BRREANDREVISILV>
2025-04-28 16:25:19 -0300

    Finalizando o controller para a entidade produtos inserindo POST, PUT e DELETE.

--- APICatalogo/Controllers/ProductsController.cs
+++ APICatalogo/Controllers/ProductsController.cs
@@ -26,7 +26,7 @@ namespace APICatalogo.Controllers
             return products;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name="ObterProduto")]
         public ActionResult<Product> Get(int id)
         {
             var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
@@ -36,5 +36,50 @@ namespace APICatalogo.Controllers
             }
             return product;
         }
+        [HttpPost]
+        public ActionResult Post(Product product)
+        {
+            if(product == null) 
+            {
+                return BadRequest("Produto inválido");
+            }
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id= product.ProductId }, product);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Product product)
+        {
+            if(id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+
     }
 }
